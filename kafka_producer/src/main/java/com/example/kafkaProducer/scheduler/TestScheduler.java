7192c75ee639a8f0a3c292815b1bb2d0be70a0e2
od <od@sweettracker.co.kr>
2023-10-24 18:21:54 +0900

    perf: KafkaProducer 개선 (push 예외 처리)

--- kafka_producer/src/main/java/com/example/kafkaProducer/scheduler/TestScheduler.java
+++ kafka_producer/src/main/java/com/example/kafkaProducer/scheduler/TestScheduler.java
@@ -2,13 +2,15 @@ package com.example.kafkaProducer.scheduler;
 
 import com.example.kafkaProducer.domain.MyData;
 import com.example.kafkaProducer.utils.JsonUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Slf4j
 @Configuration
@@ -21,7 +23,7 @@ public class TestScheduler {
     private final JsonUtil jsonUtil;
 
     public TestScheduler(KafkaTemplate<String, String> kafkaTemplate, JsonUtil jsonUtil){
-        this.topicName = "test-topic";
+        this.topicName = "od";
         this.kafkaTemplate = kafkaTemplate;
         this.jsonUtil = jsonUtil;
     }
@@ -32,8 +34,20 @@ public class TestScheduler {
             .name("od")
             .time(LocalDateTime.now().toString())
             .build());
-        log.info("[send] ==> " + message);
-        kafkaTemplate.send(topicName, message);
+
+        // 복수의 파티션은 round-robbin 으로 파티션에 전송
+        ListenableFuture<SendResult<String, String>> result = kafkaTemplate.send(topicName, message);
+
+        result.addCallback(new KafkaSendCallback<>(){
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("[send] ==> " + message);
+            }
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+                log.error("[send] failed ==> {} | {}",message, ex.toString());
+            }
+        });
     }
 
 }

1950388c9f1eb7f25f115a348100130cf4cb92b3
od <od@sweettracker.co.kr>
2023-10-24 18:22:36 +0900

    perf: kafkaConsumer 개선 (ConsumerConfig 수정)

--- kafka_consumer/src/main/java/com/example/kafkaConsumer/config/KafkaConsumerConfig.java
+++ kafka_consumer/src/main/java/com/example/kafkaConsumer/config/KafkaConsumerConfig.java
@@ -1,6 +1,5 @@
 package com.example.kafkaConsumer.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -11,32 +10,71 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory1() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "158.179.160.200:9092, 152.67.221.250:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
-        // org.apache.kafka.common.serialization.StringDeserializer
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false); // for manual commit
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    public ConsumerFactory<String, String> consumerFactory2() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "158.179.160.200:9092, 152.67.221.250:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group2");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);// for manual commit
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> batchConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "158.179.160.200:9092, 152.67.221.250:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group3");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false); // for manual commit
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100"); // 한번에 처리하는 최대 레코드 수
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory1() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
             new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerFactory1());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);// for manual commit
         return factory;
     }
 
     @Bean
-    public ObjectMapper objectMapper (){
-        return new ObjectMapper();
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory2() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory2());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // for manual commit
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> batchListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(batchConsumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // for manual commit
+        factory.setBatchListener(true); // for batchListener
+        return factory;
     }
 }
